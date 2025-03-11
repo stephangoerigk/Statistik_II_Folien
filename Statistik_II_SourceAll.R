8b4b3bb7d7a65306c52bdb8a1a3ef16599a99b9d
stephangoerigk <stephan.goerigk@gmail.com>
2025-03-11 11:45:38 +0100

    Initial commit

---
+++ Statistik_II_SourceAll.R
@@ -0,0 +1,16 @@
+library(xaringan)
+
+inf_mr("Statistik_II_1_WS24.Rmd")
+inf_mr("Statistik_II_2_WS24.Rmd")
+inf_mr("Statistik_II_3_WS24.Rmd")
+inf_mr("Statistik_II_4_WS24.Rmd")
+inf_mr("Statistik_II_5_WS24.Rmd")
+inf_mr("Statistik_II_6_WS24.Rmd")
+inf_mr("Statistik_II_7_WS24.Rmd")
+inf_mr("Statistik_II_8_WS24.Rmd")
+inf_mr("Statistik_II_9_WS24.Rmd")
+inf_mr("Statistik_II_10_WS24.Rmd")
+inf_mr("Statistik_II_11_WS24.Rmd")
+inf_mr("Statistik_II_12_WS24.Rmd")
+
+# create all htmls
